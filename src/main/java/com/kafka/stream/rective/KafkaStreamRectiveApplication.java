8b6f37fbfbd9b2323c57f5f9dd6cc82778515d1e
Denis Silveira <denis.luna@leovegas.com>
2025-03-19 15:20:01 +0100

    first commit

---
+++ src/main/java/com/kafka/stream/rective/KafkaStreamRectiveApplication.java
@@ -0,0 +1,13 @@
+package com.kafka.stream.rective;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaStreamRectiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaStreamRectiveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kafka/stream/rective/KafkaStreamRectiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kafka.stream.rective;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaStreamRectiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
